2272728f360f97d50f99eb3b115296117312ec0d
gopal <buragaddagopal345@gmail.com>
2025-08-29 16:05:46 +0530

    updated changes in ThisMethod

--- src/Car/ThisMethod.java
+++ src/Car/ThisMethod.java
@@ -1,5 +1,17 @@
 package Car;
 
 public class ThisMethod {
+	int x;
+	public ThisMethod(int x)
+	{
+		this.x=x;
+		 
+		
+	}
+	public static void main(String[] args) {
+		ThisMethod n1=new ThisMethod(21);
+		System.out.println( n1.x);
+		System.out.println("ThisMethod.main()");
+	}
 
 }
